0fc5b5b61bcb0d994c73de36db1cc825f5a474b3
berkkosal <kosalnberk@gmail.com>
2023-09-01 23:04:36 +0300

    BASE_URL verildi

--- frontend/src/services/TodoApi.js
+++ frontend/src/services/TodoApi.js
@@ -1,33 +1,33 @@
 import axios from "axios"
 
-
+const BASE_URL = "http://localhost:8080/api/"
 
 class TodoApi {
 
 
     //CREATE
     todoApiCreate(todoDto) {
-        return axios.post('http://localhost:8080/api/create', todoDto)
+        return axios.post(`${BASE_URL}create`, todoDto)
     }
 
     //LIST
     todoApiList() {
-        return axios.get('/list')
+        return axios.get(`${BASE_URL}list`)
     }
 
     //FIND
     todoApiFindById(id) {
-        return axios.get(`/find/${id}`)
+        return axios.get(`${BASE_URL}find/${id}`)
     }
 
     //UPDATE
     todoApiUpdate(id, todoDto) {
-        return axios.put(`/update/${id}`, todoDto)
+        return axios.put(`${BASE_URL}update/${id}`, todoDto)
     }
 
     //DELETE
     todoApiDelete(id) {
-        return axios.delete(`/delete/${id}`)
+        return axios.delete(`${BASE_URL}delete/${id}`)
     }
 
     // delete all todo
