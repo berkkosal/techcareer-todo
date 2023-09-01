490a83620ab3cc4d8011caa2d8661ffb1eccc9ed
berkkosal <kosalnberk@gmail.com>
2023-09-01 11:31:37 +0300

    Update TodoDto.java

--- src/main/java/com/kosalberk/todo/model/dto/TodoDto.java
+++ src/main/java/com/kosalberk/todo/model/dto/TodoDto.java
@@ -1,4 +1,12 @@
 package com.kosalberk.todo.model.dto;
 
+import lombok.Data;
+
+import java.util.UUID;
+@Data
 public class TodoDto {
+    private UUID uuid;
+    private String title;
+    private String description;
+
 }
