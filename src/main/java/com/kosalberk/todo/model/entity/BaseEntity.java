fbc2f4233b7b59ca869e3a856dff35eabf4d5878
berkkosal <kosalnberk@gmail.com>
2023-09-01 12:04:31 +0300

    docker-compose

--- src/main/java/com/kosalberk/todo/model/entity/BaseEntity.java
+++ src/main/java/com/kosalberk/todo/model/entity/BaseEntity.java
@@ -1,4 +1,26 @@
 package com.kosalberk.todo.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@MappedSuperclass
+@Data
 public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column
+    private UUID uuid;
+
+    @CreationTimestamp
+    private Timestamp createdTime;
+    @UpdateTimestamp
+    private Timestamp updatedTime;
+    @Column
+    private Timestamp deletedTime;
 }
