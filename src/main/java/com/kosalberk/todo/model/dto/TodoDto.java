78257cc1857eb365089c25be092f7c5e36a4be29
berkkosal <kosalnberk@gmail.com>
2023-09-01 17:50:39 +0300

    update tododto,todo

--- src/main/java/com/kosalberk/todo/model/dto/TodoDto.java
+++ src/main/java/com/kosalberk/todo/model/dto/TodoDto.java
@@ -1,12 +1,11 @@
 package com.kosalberk.todo.model.dto;
 
+import com.kosalberk.todo.model.entity.BaseEntity;
+import jakarta.persistence.Entity;
 import lombok.Data;
 
 import java.util.UUID;
 @Data
+@Entity
 public class TodoDto {
-    private UUID uuid;
-    private String title;
-    private String description;
-
 }
--- src/main/java/com/kosalberk/todo/model/entity/Todo.java
+++ src/main/java/com/kosalberk/todo/model/entity/Todo.java
@@ -1,4 +1,12 @@
 package com.kosalberk.todo.model.entity;
+import jakarta.persistence.Entity;
+import lombok.Data;
 
-public class Todo {
+import java.util.UUID;
+@Data
+@Entity
+public class Todo extends BaseEntity {
+    private String title;
+    private String description;
+
 }
